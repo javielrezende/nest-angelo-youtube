58f8fd13c16aa640d0deac2cd9bd29cb34505260
Roger Rezende <javielrezende@gmail.com>
2020-08-02 20:55:56 -0300

    :art: Config database and typeorm
    Config database and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 3306,
+      username: 'javielrezende',
+      password: '',
+      database: 'angeloyt',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
