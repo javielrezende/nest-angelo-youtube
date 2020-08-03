de2503ed53cae4d26922bf70637b85520ed97d0d
Roger Rezende <javielrezende@gmail.com>
2020-08-02 23:21:27 -0300

    :bug: Fix removing config and change db port

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,7 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 3306,
-      username: 'javielrezende',
-      password: '',
-      database: 'angeloyt',
-      entities: [],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(),
     UserModule,
   ],
   controllers: [AppController],
